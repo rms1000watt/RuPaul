ce99d879fcfd21bba993e5c7ec4e72005fd947d8
Ryan Smith <rsmith@lanterncredit.com>
2017-04-19 08:44:02 -0700

    Rearranged generate cmd

--- cmd/generate.go
+++ cmd/generate.go
@@ -16,24 +16,26 @@ var (
 		Use:   "generate",
 		Short: "Generates code from a `rygen.yml` file",
 		Long:  `Generates code from a "rygen.yml" file`,
-		Run: func(cmd *cobra.Command, args []string) {
-			genCfg := generate.Config{}
-
-			fileBytes, err := ioutil.ReadFile(configFilePath)
-			if err != nil {
-				fmt.Println("Failed reading config:", err)
-				return
-			}
-
-			if err := yaml.Unmarshal(fileBytes, &genCfg); err != nil {
-				fmt.Println("Failed unmarshalling Yaml:", err)
-				return
-			}
-
-			generate.Generate(genCfg)
-		},
+		Run:   runGenerate,
 	}
 )
+
+func runGenerate(cmd *cobra.Command, args []string) {
+	genCfg := generate.Config{}
+
+	fileBytes, err := ioutil.ReadFile(configFilePath)
+	if err != nil {
+		fmt.Println("Failed reading config:", err)
+		return
+	}
+
+	if err := yaml.Unmarshal(fileBytes, &genCfg); err != nil {
+		fmt.Println("Failed unmarshalling Yaml:", err)
+		return
+	}
+
+	generate.Generate(genCfg)
+}
 
 func init() {
 	RootCmd.AddCommand(generateCmd)
